931a0e55030bfbc63c81f1adecf0055fc23fa421
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-11-14 00:13:32 -0300

    feat: adding pokemons imgs

--- src/routes/pokemons.tsx
+++ src/routes/pokemons.tsx
@@ -5,14 +5,21 @@ import { useStore } from "@tanstack/react-store";
 export const Route = createFileRoute("/pokemons")({
   component: RouteComponent,
   loader: async () => {
-      const page = pageStore.state;
-    const offset = page * 20
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`);
+    const page = pageStore.state;
+    const offset = page * 50;
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=50`
+    );
     return response.json() as Promise<{
       results: { name: string; url: string }[];
     }>;
   },
 });
+
+function getImageFromUrl(url: string) {
+  const id = url.split("/").filter(Boolean).pop();
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+}
 
 /*TODO call "https://swapi.dev/api/people" */
 function RouteComponent() {
@@ -22,11 +29,17 @@ function RouteComponent() {
   console.log("getting data - ", data);
   return (
     <div>
-      Hello "/pokemons"!
       Page - {page}
-      {data.results.map((pokemon) => {
-        return <div key={pokemon.name}>{pokemon.name}</div>;
-      })}
+      <div className="flex flex-wrap">
+        {data.results.map((pokemon) => {
+          return (
+            <div key={pokemon.name}>
+              <span className="text-red-400 font-semibold">{pokemon.name}</span>
+              <img className="w-20" src={getImageFromUrl(pokemon.url)} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
