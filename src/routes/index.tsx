989af2ccea9bb902c7025b9b4930d519cd646b4a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-11-14 00:08:08 -0300

    feat: added next page button into home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,21 @@
+import { pageStore } from "@/stores/pageStore";
 import { createFileRoute } from "@tanstack/react-router";
 import { Link } from "@tanstack/react-router";
+import { useStore } from "@tanstack/react-store";
+
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+  const page = useStore(pageStore);
   return (
-    <div className="flex items-start gap-2 p-8 min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900">
+    <div className="flex items-start gap-2 p-8 min-h-screen bg-linear-to-b from-slate-900 via-slate-800 to-slate-900">
+      <button
+        className="px-8 py-3 bg-cyan-500 hover:bg-cyan-600 text-white font-semibold rounded-lg transition-colors shadow-lg shadow-cyan-500/50"
+        onClick={() => pageStore.setState((n) => n + 1)}
+      >
+        Next page - {page}
+      </button>
       <Link
         className="px-8 py-3 bg-cyan-500 hover:bg-cyan-600 text-white font-semibold rounded-lg transition-colors shadow-lg shadow-cyan-500/50"
         to="/pokemons"
