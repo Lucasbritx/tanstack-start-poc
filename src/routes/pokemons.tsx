6be2bc456a82476d3484c60cd9f691b7bd8a892a
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-11-14 00:08:30 -0300

    feat: fetching pokemons based on page store(added pagination)

--- src/routes/pokemons.tsx
+++ src/routes/pokemons.tsx
@@ -1,9 +1,13 @@
+import { pageStore } from "@/stores/pageStore";
 import { createFileRoute } from "@tanstack/react-router";
+import { useStore } from "@tanstack/react-store";
 
 export const Route = createFileRoute("/pokemons")({
   component: RouteComponent,
   loader: async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const page = pageStore.state;
+    const offset = page * 20
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`);
     return response.json() as Promise<{
       results: { name: string; url: string }[];
     }>;
@@ -12,11 +16,14 @@ export const Route = createFileRoute("/pokemons")({
 
 /*TODO call "https://swapi.dev/api/people" */
 function RouteComponent() {
+  const page = useStore(pageStore);
+
   const data = Route.useLoaderData();
   console.log("getting data - ", data);
   return (
     <div>
       Hello "/pokemons"!
+      Page - {page}
       {data.results.map((pokemon) => {
         return <div key={pokemon.name}>{pokemon.name}</div>;
       })}
